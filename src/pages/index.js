184a6bdce1919e3e0f0db11ac0e4bd6aaafa717c
Ken Rogers <ken@kenrogers.co>
2019-04-17 19:01:19 -0600

    Add donation form

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,28 @@
-import React from 'react'
+import React from "react";
 
-import Layout from '../components/layout'
-import SEO from '../components/seo'
-import StripeCheckout from 'react-stripe-checkout'
-import axios from 'axios'
-import uuid from 'uuid/v4'
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
+import uuid from "uuid/v4";
 
 class IndexPage extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      thanks: ''
-    }
-    this.onToken = this.onToken.bind(this)
+      thanks: ""
+    };
+    this.onToken = this.onToken.bind(this);
   }
   async onToken(token) {
-    const json = await axios.post('/.netlify/functions/donate', {
+    const json = await axios.post("/.netlify/functions/donate", {
       token,
       idempotency_key: uuid()
-    })
-    if (json.data.status === 'succeeded') {
+    });
+    if (json.data.status === "succeeded") {
       this.setState({
         thanks: "🎉 Thank you for your donation! You're awesome! 🎉"
-      })
+      });
     }
   }
   render() {
@@ -33,7 +33,22 @@ class IndexPage extends React.Component {
         <div className="text-center">
           <h1 className="mb-4">Donate to El Faro</h1>
 
-          <p>Future home of the donation page for El Faro Orphanage.</p>
+          <script src="https://donorbox.org/widget.js" paypalExpress="false" />
+          <iframe
+            src="https://donorbox.org/embed/el-faro-orphanage"
+            height="685px"
+            width="100%"
+            style={{
+              maxWidth: "425px",
+              minWidth: "310px",
+              maxHeight: "none!important"
+            }}
+            seamless="seamless"
+            name="donorbox"
+            frameborder="0"
+            scrolling="no"
+            allowpaymentrequest
+          />
           {/* <br />
           {this.amount > 0 ? (
             <StripeCheckout
@@ -48,8 +63,8 @@ class IndexPage extends React.Component {
           <p>{this.state.thanks ? this.state.thanks : ''}</p> */}
         </div>
       </Layout>
-    )
+    );
   }
 }
 
-export default IndexPage
+export default IndexPage;
